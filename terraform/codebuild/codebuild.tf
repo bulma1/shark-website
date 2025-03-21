93e96230ba7e673bbd2aa724e6437b8cd314b100
Jason <16521335@gm.uit.edu.vn>
2025-03-21 09:43:40 +0700

    Add codebuild terraform

---
+++ terraform/codebuild/codebuild.tf
@@ -0,0 +1,33 @@
+resource "aws_codebuild_project" "shark_build" {
+  name          = "${var.app_name}-${var.environment}-build"
+  build_timeout = 60
+  description   = "Build project of shark website"
+  service_role  = aws_iam_role.codebuild_role.arn
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:5.0" # Includes Node.js and Docker
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true # Required for Docker builds
+    environment_variable {
+      name  = "AWS_REGION"
+      value = "us-east-2" # Matches your region
+    }
+    environment_variable {
+      name  = "ECR_REPO"
+      value = data.aws_ecr_repository.shark-website.repository_url
+    }
+  }
+  source {
+    type                = "GITHUB"
+    location            = "https://github.com/bulma1/shark-website.git" # Your repo
+    git_clone_depth     = 1
+    buildspec           = "buildspec.yml"
+    insecure_ssl        = false
+    report_build_status = false
+  }
+  # Webhook to trigger builds on GitHub push
+  source_version = "main" # Adjust if your default branch is different
+}
--- terraform/codebuild/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ecr_url" {
-  value = aws_ecr_repository.shark-website.repository_url
-}
