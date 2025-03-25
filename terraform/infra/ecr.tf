a06062a7c1af7a1329df9e2511d0804589d006ea
Jason <16521335@gm.uit.edu.vn>
2025-03-25 10:03:35 +0700

    Add rule for ECR

--- terraform/infra/ecr.tf
+++ terraform/infra/ecr.tf
@@ -6,3 +6,24 @@ resource "aws_ecr_repository" "shark-website" {
     scan_on_push = true
   }
 }
+resource "aws_ecr_lifecycle_policy" "shark_website_policy" {
+  repository = aws_ecr_repository.shark-website.name
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep only the last 5 images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 5
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
