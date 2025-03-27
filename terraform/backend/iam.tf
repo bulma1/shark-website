8e855cde35fe634ee19b1728dece02bb9ca0977d
Jason <16521335@gm.uit.edu.vn>
2025-03-27 10:12:08 +0700

    Fix policy

--- terraform/backend/iam.tf
+++ terraform/backend/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ec2_role" {
-  name = "${var.provider_name}-${var.environment}-${var.app_name}-ec2-role"
+  name = "${var.provider_name}-${var.environment}-${var.app_name}-ec2-codedeploy-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -44,6 +44,6 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_attachment" {
 }
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "${var.provider_name}-${var.environment}-${var.app_name}-ec2-profile"
+  name = "${var.provider_name}-${var.environment}-${var.app_name}-ec2-codedeploy-profile"
   role = aws_iam_role.ec2_role.name
 }
