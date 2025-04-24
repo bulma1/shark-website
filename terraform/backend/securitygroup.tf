cca6d63237e1c89e922b0b395f4e51c77a6e0d08
Jason <16521335@gm.uit.edu.vn>
2025-04-24 09:19:44 +0700

    fix: change port in sg for test v0.2

--- terraform/backend/securitygroup.tf
+++ terraform/backend/securitygroup.tf
@@ -27,8 +27,8 @@ resource "aws_security_group" "shark_sg" {
     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
   }
   ingress {
-    from_port   = 3009
-    to_port     = 3009
+    from_port   = 3010
+    to_port     = 3010
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
   }
