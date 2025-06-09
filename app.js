fc24babc389b2a03a40f5f7d82086ef40887d706
Jason <16521335@gm.uit.edu.vn>
2025-06-09 11:35:47 +0700

    fix: fix port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const path = __dirname + '/views/';
-const port = 8089;
+const port = 8080;
 const winston = require('winston');
 
 // List of example users
--- terraform/backend/securitygroup.tf
+++ terraform/backend/securitygroup.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "shark_sg" {
   description = "Allow HTTP and SSH traffic"
 
   ingress {
-    from_port   = 8089
-    to_port     = 8089
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
   }
